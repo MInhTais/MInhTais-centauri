38d772f2b5566ddbcf14ad011ea01ed7a196be46
MInhTais <minnhtai2019cb2@gmail.com>
2024-08-27 17:40:44 +0700

    fix config db to server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import express from 'express';
 import db from "../db/db_connect"
 import path from 'path';
@@ -7,9 +8,9 @@ import authRoutes from './routes/auth,route';
 
 const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/auth', authRoutes);
 db
 
